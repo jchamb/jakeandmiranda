d9bc539b374b03e2b1058610e1747264ec9bbfd1
Jake Chamberlain <jake@jchamb.com>
2014-04-24 21:19:06 -0400

    add sass and ignore

---
+++ config.rb
@@ -0,0 +1,7 @@
+javascripts_dir = 'www/assets/js/'
+css_dir = 'www/assets/css/'
+sass_dir = 'www/assets/sass/'
+images_dir = 'www/assets/images/'
+fonts_dir = 'www/assets/fonts/'
+
+output_style = :compressed
