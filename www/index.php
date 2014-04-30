933cc7eac5b357c42be3f841c8bffd5c4378bdc8
Jake Chamberlain <jake@jchamb.com>
2014-04-30 00:19:43 -0400

    add info screen with base styles

--- www/index.php
+++ www/index.php
@@ -18,8 +18,8 @@
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
-        <!--<article class="site__wrapper">-->
-            <section class="intro">
+        <article class="site__wrapper">
+            <section class="section intro visible">
                 <div class="intro__image intro__image--one"></div>
                 <div class="intro__image intro__image--two"></div>
                 <div class="intro__image intro__image--three"></div>
@@ -32,24 +32,40 @@
                     <p>Life + Love</p>
                 </div>
             </section>
-            <section class="about">
-                
+            
+            <section class="section location visible">
+                <div class="left">
+                    <header class="callout">
+                        <p>Miranda + Jake</p>
+                        <p>are engaged to be married</p>
+                        <p><span class="line"></span>at<span class="line"></span></p>
+                        <p>Salem Stone Church, Carlisle, PA</p>
+                    </header>
+                    <section class="about">
+                    </section>
+                </div>
+                <div class="right">
+                    <section class="buttons">
+                        
+                    </section>
+                    <section class="map" id="map">
+                    </section>    
+                </div>
             </section>
-            <section class="location">
-                
+            <section class="section register visible">
+                some text
             </section>
-            <section class="gallery">
-                
+            <section class="section gallery visible">
+                some text
             </section>
-            <section class="register">
-                
-            </section>
-        <!--</article>-->
+            
+        </article>
 
         
 
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=true"></script>
         <script src="/assets/js/plugins.js"></script>
         <script src="/assets/js/main.js"></script>
 
